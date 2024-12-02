3ea685e0a6d4170e081afef1e18ef1b9277ae7af
Azima <azimaqadirli@gmail.com>
2024-12-02 18:41:28 +0400

    Updated

--- FarmToFork/Areas/Admin/Controllers/FeatureController.cs
+++ FarmToFork/Areas/Admin/Controllers/FeatureController.cs
@@ -20,10 +20,53 @@ public class FeatureController : Controller
         var models = await _repository.GetAll().ToListAsync();
         return View(models);
     }
-
+[HttpGet]
     public IActionResult Add()
     {
         return View();
     }
 
+    [HttpPost]
+    public async Task<IActionResult> Add(Feature feature)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        await _repository.AddAsync(feature);
+        await _repository.SaveAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Update(int id)
+    {
+        
+        var feature = await _repository.GetAsync(id);
+        return View(feature);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(int id,Feature feature)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(); 
+        }
+        var updatedFeature = await _repository.GetAsync(id);
+        updatedFeature.Title = feature.Title;
+        updatedFeature.Description = feature.Description;
+        updatedFeature.Icon = feature.Icon;
+        
+        _repository.Update(updatedFeature);
+        await _repository.SaveAsync();
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Remove(int id)
+    {
+        await _repository.RemoveAsync(id);
+        await _repository.SaveAsync();
+        return RedirectToAction("Index");
+    }
 }
--- FarmToFork/Controllers/HomeController.cs
+++ FarmToFork/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
+using FarmToFork.Models;
+using FarmToFork.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FarmToFork.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IRepository<Feature> _repository;
 
-    public IActionResult Index()
+    public HomeController(IRepository<Feature> repository)
     {
-        return View();
+        _repository = repository;
+    }
+
+    public  async Task<IActionResult> Index()
+    {
+        var response = await _repository.GetAll().ToListAsync();
+        return View(response);
     }
 
     
